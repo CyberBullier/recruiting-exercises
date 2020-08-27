add4542ebad5996350210796667ae937aecc0018
theyoungkai <Kai Lee>
2020-08-27 13:59:40 -0400

    removed irrelevant testcase to accomodate modified cheapest_shipment function

--- inventory-allocator/inventory_allocator_test.py
+++ inventory-allocator/inventory_allocator_test.py
@@ -5,79 +5,33 @@ from main import cheapest_shipment
 class InventoryAllocatorTest(unittest.TestCase):
     def test_empty_arguments(self):
         """
-        tests and/or arguments are empty
+        tests for empty arguments
         """
-        items, warehouses = {}, [{"name": "w1", "inventory": {"apple": 3}}]
+        all_orders, warehouses = {}, [{"name": "w1", "inventory": {"apple": 3}}]
         expected = []
+        # assert when empty all_orders
         self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(all_orders, warehouses)
         )
-        items, warehouses = {"babycow": 4}, []
+        all_orders, warehouses = {"babycow": 4}, []
+        # assert when no warehouses
         self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(all_orders, warehouses)
         )
-        items, warehouses = {}, []
+        all_orders, warehouses = {}, []
+        # assert when no orders and warehouses
         self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(all_orders, warehouses)
         )
 
     def test_no_item_in_warehouse(self):
         """
         tests when an order item isn't present in any warehouse
         """
-        warehouses, items = [{"name": "w1", "inventory": {"apple": 3}}], {"babycow": 3}
+        warehouses, all_orders = [{"name": "w1", "inventory": {"apple": 3}}], {"babycow": 3}
         expected = []
         self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
-        )
-
-    def test_item_in_warehouse_not_enough(self):
-        """
-        tests when order item is present in warehouse but insufficient inventory
-
-        """
-        warehouses = [
-            {"name": "w1", "inventory": {"apple": 3}},
-            {"name": "w2", "inventory": {"babycow": 3}},
-        ]
-        items = {"babycow": 5}
-        expected = []
-        self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
-        )
-
-    def test_item_in_many_warehouse_not_enough_and_enough(self):
-        """
-        tests order items that have sufficient and insufficient inventory
-        across many warehouses
-        """
-        warehouses = [
-            {"name": "w1", "inventory": {"babycow": 5, "catherine": 4}},
-            {"name": "w2", "inventory": {"babycow": 3, "catherine": 1}},
-        ]
-        items = {"babycow": 6, "catherine": 8}
-        expected = [{"w1": {"babycow": 5}}, {"w2": {"babycow": 1}}]
-        self.assertEqual(
-            sorted(expected, key=lambda x: x.keys()),
-            sorted(
-                cheapest_shipment(items, warehouses),
-                key=lambda x: x.keys(),
-            ),
-        )
-
-    def test_no_item_in_many_warehouse_and_not_enough(self):
-        """
-        tests when order items are either non existent(order item name
-        not in warehouse) and/or insufficient inventory
-        """
-        warehouses = [
-            {"name": "w1", "inventory": {"babycow": 5, "catherine": 4}},
-            {"name": "w2", "inventory": {"babycow": 3, "catherine": 1}},
-        ]
-        items = {"babycow": 9, "katherine": 8}
-        expected = []
-        self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(all_orders, warehouses)
         )
 
     def test_zero_item_count(self):
@@ -87,11 +41,61 @@ class InventoryAllocatorTest(unittest.TestCase):
         warehouses = [
             {"name": "w1", "inventory": {"babycow": 0, "catherine": 4}},
         ]
-        items = {"babycow": 9}
+        all_orders = {"babycow": 9}
         expected = []
         self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(all_orders, warehouses)
         )
+    def test_item_in_warehouse_not_enough(self):
+        """
+        tests when order item is present in warehouse, its count
+        is greater than 0 but insufficient inventory
+
+        """
+        warehouses = [
+            {"name": "w1", "inventory": {"apple": 3}},
+            {"name": "w2", "inventory": {"babycow": 3}},
+        ]
+        all_orders = {"babycow": 5}
+        expected = []
+        self.assertEqual(
+            expected, cheapest_shipment(all_orders, warehouses)
+        )
+
+    def test_item_in_many_warehouse_not_enough_and_enough(self):
+        """
+        tests all_orders items that have sufficient and insufficient inventory
+        across many warehouses
+        """
+        warehouses = [
+            {"name": "w1", "inventory": {"babycow": 5, "catherine": 4}},
+            {"name": "w2", "inventory": {"babycow": 3, "catherine": 1}},
+        ]
+        all_orders = {"babycow": 6, "catherine": 8}
+        expected = [{"w1": {"babycow": 5}}, {"w2": {"babycow": 1}}]
+        self.assertEqual(
+            sorted(expected, key=lambda x: x.keys()),
+            sorted(
+                cheapest_shipment(all_orders, warehouses),
+                key=lambda x: x.keys(),
+            ),
+        )
+
+    def test_no_item_in_many_warehouse_and_not_enough(self):
+        """
+        tests when all_orders items are either non existent(order item name
+        not in warehouse) and/or insufficient inventory
+        """
+        warehouses = [
+            {"name": "w1", "inventory": {"babycow": 5, "catherine": 4}},
+            {"name": "w2", "inventory": {"babycow": 3, "catherine": 1}},
+        ]
+        all_orders = {"babycow": 9, "katherine": 8}
+        expected = []
+        self.assertEqual(
+            expected, cheapest_shipment(all_orders, warehouses)
+        )
+
 
     def test_order_cheapest_shipment_single_warehouse(self):
         """
@@ -101,10 +105,10 @@ class InventoryAllocatorTest(unittest.TestCase):
             {"name": "w1", "inventory": {"babycow": 5}},
             {"name": "w2", "inventory": {"babycow": 3}},
         ]
-        items = {"babycow": 4}
+        all_orders = {"babycow": 4}
         expected = [{"w1": {"babycow": 4}}]
         self.assertEqual(
-            expected, cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(all_orders, warehouses)
         )
 
     def test_order_cheapest_shipment_many_warehouse(self):
@@ -115,12 +119,12 @@ class InventoryAllocatorTest(unittest.TestCase):
             {"name": "w1", "inventory": {"babycow": 5}},
             {"name": "w2", "inventory": {"babycow": 3}},
         ]
-        items = {"babycow": 6}
+        all_orders = {"babycow": 6}
         expected = [{"w1": {"babycow": 5}}, {"w2": {"babycow": 1}}]
         self.assertEqual(
             sorted(expected, key=lambda x: x.keys()),
             sorted(
-                cheapest_shipment(items, warehouses),
+                cheapest_shipment(all_orders, warehouses),
                 key=lambda x: x.keys(),
             ),
         )
