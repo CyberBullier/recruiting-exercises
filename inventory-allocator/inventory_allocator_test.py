4f0d634d8ae568c67f6085541710f8b4f27814c3
theyoungkai <Kai Lee>
2020-08-27 04:37:52 -0400

    refactored original solution at the discretion of updated README.md on original repository

--- inventory-allocator/inventory_allocator_test.py
+++ inventory-allocator/inventory_allocator_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import InventoryAllocator
+from main import cheapest_shipment
 
 class InventoryAllocatorTest(unittest.TestCase):
     def test_empty_arguments(self):
@@ -10,15 +10,15 @@ class InventoryAllocatorTest(unittest.TestCase):
         items, warehouses = {}, [{"name": "w1", "inventory": {"apple": 3}}]
         expected = []
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
         items, warehouses = {"babycow": 4}, []
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
         items, warehouses = {}, []
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
 
     def test_no_item_in_warehouse(self):
@@ -28,7 +28,7 @@ class InventoryAllocatorTest(unittest.TestCase):
         warehouses, items = [{"name": "w1", "inventory": {"apple": 3}}], {"babycow": 3}
         expected = []
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
 
     def test_item_in_warehouse_not_enough(self):
@@ -43,12 +43,12 @@ class InventoryAllocatorTest(unittest.TestCase):
         items = {"babycow": 5}
         expected = []
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
 
     def test_item_in_many_warehouse_not_enough_and_enough(self):
         """
-        tests order items that have sufficient and insufficient inventory 
+        tests order items that have sufficient and insufficient inventory
         across many warehouses
         """
         warehouses = [
@@ -60,14 +60,15 @@ class InventoryAllocatorTest(unittest.TestCase):
         self.assertEqual(
             sorted(expected, key=lambda x: x.keys()),
             sorted(
-                InventoryAllocator().cheapest_shipment(items, warehouses),
+                cheapest_shipment(items, warehouses),
                 key=lambda x: x.keys(),
             ),
         )
 
     def test_no_item_in_many_warehouse_and_not_enough(self):
         """
-        tests when order items are either non existent and/or not enough
+        tests when order items are either non existent(order item name
+        not in warehouse) and/or insufficient inventory
         """
         warehouses = [
             {"name": "w1", "inventory": {"babycow": 5, "catherine": 4}},
@@ -76,7 +77,7 @@ class InventoryAllocatorTest(unittest.TestCase):
         items = {"babycow": 9, "katherine": 8}
         expected = []
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
 
     def test_zero_item_count(self):
@@ -85,31 +86,16 @@ class InventoryAllocatorTest(unittest.TestCase):
         """
         warehouses = [
             {"name": "w1", "inventory": {"babycow": 0, "catherine": 4}},
-            {"name": "w2", "inventory": {"babycow": 0, "catherine": 1}},
         ]
         items = {"babycow": 9}
         expected = []
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
-        )
-
-    def test_no_empty_warehouse_in_reponse(self):
-        """
-        tests when warehouse only has target item but item inventory is insufficient across all warehouses
-        """
-        warehouses = [
-            {"name": "w1", "inventory": {"babycow": 5, "catherine": 4}},
-            {"name": "w2", "inventory": {"babycow": 3, "catherine": 1}},
-        ]
-        items = {"babycow": 5, "catherine": 8}
-        expected = [{"w1": {"babycow": 5}}]
-        self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
 
     def test_order_cheapest_shipment_single_warehouse(self):
         """
-        test cheapest shipment holds true
+        test correct cheapest shipment warehouse is selected
         """
         warehouses = [
             {"name": "w1", "inventory": {"babycow": 5}},
@@ -118,12 +104,12 @@ class InventoryAllocatorTest(unittest.TestCase):
         items = {"babycow": 4}
         expected = [{"w1": {"babycow": 4}}]
         self.assertEqual(
-            expected, InventoryAllocator().cheapest_shipment(items, warehouses)
+            expected, cheapest_shipment(items, warehouses)
         )
 
     def test_order_cheapest_shipment_many_warehouse(self):
         """
-        test cheapest shipment holds true split across many warehouses
+        test optimality of warehouse(s) selected and their split
         """
         warehouses = [
             {"name": "w1", "inventory": {"babycow": 5}},
@@ -134,7 +120,7 @@ class InventoryAllocatorTest(unittest.TestCase):
         self.assertEqual(
             sorted(expected, key=lambda x: x.keys()),
             sorted(
-                InventoryAllocator().cheapest_shipment(items, warehouses),
+                cheapest_shipment(items, warehouses),
                 key=lambda x: x.keys(),
             ),
         )
--- inventory-allocator/main.py
+++ inventory-allocator/main.py
@@ -1,49 +1,42 @@
 from typing import List, Dict
 import collections
 
-class InventoryAllocator:
-    def _post_process_shipment(self, shipment: Dict[str,Dict[str,int]]) -> List[Dict[str,Dict[str,int]]]:
-        ans = []
-        for key,val in shipment.items():
-            if not val:
-                continue
-            else:
-                ans.append({key:val})
-        return ans
 
-    def cheapest_shipment(self, items: Dict[str, int], warehouses: List[Dict]) -> List[Dict[str,Dict[str,int]]]:
-        """
-            preconditions:
-                - assume warehouses presorted based on cost
-                - assume integer arguments are 32 bit signed, <=2^32 -1
-                - assume integer arguments in items are > 0; invalid order if ordering <= 0 of an item
-                - assume integer arguments in warehouses >= 0
-                - assume every top level dictionary object in warehouses have distinct
-                'name' value associated with its 'name' key
-        """
-        # either arguments are empty, impossible to fufill a shipment
-        if not items or not warehouses:return []
-        # initialize shipment container
-        shipment = collections.defaultdict(dict)
-        for key,val in items.items():
-            count = 0
-            # greedily select items from lowest cost warehouse to next
-            for warehouse in warehouses:
-                if key in warehouse['inventory']:
-                    if warehouse['inventory'][key] + count >=val:
-                        shipment[warehouse['name']][key] = val-count
-                        count = val
-                        break
-                    else:
-                        shipment[warehouse['name']][key] = warehouse['inventory'][key]
-                        count+=warehouse['inventory'][key]
-            if count != val:
-                #order for this item cannot be fufilled, remove from shipment
-                for item in list(shipment):
-                    if key in shipment[item]:
-                        del shipment[item][key]
-        # post process shipment results; remove warehouses with no orders
-        return self._post_process_shipment(shipment)
+def cheapest_shipment(all_orders: Dict[str, int], warehouses: List[Dict]) -> List[Dict[str,Dict[str,int]]]:
+    """
+        preconditions:
+            - assume warehouses presorted based on cost
+            - assume integer arguments are 32 bit signed, <=2^32 -1
+            - assume integer arguments in items are > 0; invalid order if ordering <= 0 of an item
+            - assume integer arguments in warehouses >= 0
+            - assume every top level dictionary object in warehouses have distinct
+            'name' value associated with its 'name' key
+            -assume valid schema for input
+    """
+    # either arguments are empty, impossible to fufill a shipment
+    if not all_orders or not warehouses:return []
+    # initialize global shipment container
+    shipment = collections.defaultdict(dict)
+    for key,val in all_orders.items():
+        count = 0
+        # initialize shipment container for warehouse & invetory for order item key
+        item_warehouse_shipments = collections.defaultdict(dict)
+        # greedily select items from lowest cost warehouse to next
+        for warehouse in warehouses:
+            if key in warehouse['inventory'] and warehouse['inventory'][key]>0:
+                if warehouse['inventory'][key] + count >=val:
+                    item_warehouse_shipments[warehouse['name']][key] = val-count
+                    count = val
+                    break
+                else:
+                    item_warehouse_shipments[warehouse['name']][key] = warehouse['inventory'][key]
+                    count+=warehouse['inventory'][key]
+        #update global shipments if current order has sufficient inventory
+        if val == count:
+            for name,warehouse_order in item_warehouse_shipments.items():
+                shipment[name].update(warehouse_order)
+
+    return [{key:val} for key,val in shipment.items()]
 
 
 
